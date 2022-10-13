ab6dbe617ce7e6bb0ad55e4679494dac6d598ca8
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-10-13 18:54:59 +0100

    Update 5-print.c

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * this prints using the printf function.
+ * It always returns 0 [return (0)] 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
