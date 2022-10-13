943346a64972629087289a08e79c3077e56c7751
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-10-13 18:28:11 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+
+/*
+*This program print "Programming is like building a multilingual puzzle" 
+*It also returns 0 [return (0)] 
+*/
+
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
