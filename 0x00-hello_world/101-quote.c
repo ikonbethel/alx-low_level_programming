bdcbb92d6401af8d2d3b183219417a46d9d8589f
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-10-14 05:03:37 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
 
 /**
  *  main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *      
+ *  
  *  followed by a new line, to standard error.
- *    
+ *  
  *  Return: Always 1.
  */
 int main(void)
