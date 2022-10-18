9f5498bb76bbbcd4be33e0e549c343879efe9b00
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-10-18 17:41:35 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * main - This program prints the phrase _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char string_[] = "_putchar";
+	int i;
+
+	for (i = 0; string_[i] != '\n'; i++)
+	{
+		_putchar(string_[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
