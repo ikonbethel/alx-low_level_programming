55b641a616698d3dc6596e5eaa104c1f87043c9a
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-10-21 02:41:01 +0100

    Update 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - This code prints lines diagonally.
+ * @n: input variable i.e for a given number of times
+ *
+ * Return: returns void
+ */
+
+
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (x = 0; x < n; x++)
+	{
+		for (y = 0; y < i; y++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+
+}
