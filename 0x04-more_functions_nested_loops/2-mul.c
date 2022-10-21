38439ff22e0cf5da2759bef0a63d7f9ab3a99c8d
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-10-21 02:07:18 +0100

    Update 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - This code multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: returns product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
