dc0d2bf62c333a576eb98339a3d9e21a227d6de1
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-10-21 05:36:08 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,18 +12,18 @@
  */
 int main(void)
 {
-	int x;
+	int i;
 
-	for (x = 1; x <= 100; x++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (x % 3 == 0 && x % 5 == 0 )
+		if (i % 15 == 0 )
 			printf("FizzBuzz");
-		else if (x % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
-		else if (x % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
-			printf("%i", x);
+			printf("%i", i);
 		if (x < 100)
 			printf(" ");
 	}
