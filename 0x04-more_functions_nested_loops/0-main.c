3828c2e1f384b40388cfa90beaa61cf23c2bd94d
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-10-21 01:52:09 +0100

    Update 0-main.c

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - This function checks for uppercase character.
+ * @c: The variable return type is int
+ * Return: This returns 1 for uppercase and  0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >=  'A' && c <= 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
