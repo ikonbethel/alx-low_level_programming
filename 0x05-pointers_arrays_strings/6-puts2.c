6fe058312f13d6730597fadb96b240f8f3d4b27d
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-10-25 01:47:19 +0100

    Update 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints every alternating character of a string
+ * @str: the string input
+ *
+ */
+void puts2(char *str)
+{
+	int x, y;
+
+	for (x = 0 ; str[x] != '\0' ; x++)
+	{
+		;
+	}
+	for (y = 0 ; y < x ; y = y + 2)
+	{
+		_putchar(str[y]);
+	}
+	_putchar('\n');
+}
