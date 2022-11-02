ed8bc0c9cd54184f0ea08b2939de91b8010c4cb4
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-11-02 18:08:53 +0100

    Update 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts_recursion - prints a string input followed by a new line
+* @s: pointer to the string input
+* Return: returns void
+*/
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
