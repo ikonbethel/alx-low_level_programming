9925ddd06bdda2d5f5de14f52dbf2bd093332f3f
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-11-05 06:06:09 +0100

    Update main.h

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_string(char *str);
+void print_number(int num);
+void print_array_elements(char *str);
+int _atoi(char *s);
+int multiply_two_nums(int num1, int num2);
+void print_number(int n);
+int _putchar(char c);
+
+
+#endif
