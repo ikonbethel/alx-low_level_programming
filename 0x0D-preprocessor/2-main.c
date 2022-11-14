91f19743c65ae6c296beeeddb0cd3560b38c5b56
ikonbethel <ikonbethel@gmail.com>
2022-11-14 16:27:16 +0100

    upload od answers on 0x0D tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point- print name of file from which it is compiled from
+* Return: returns (0) as succesful
+*/
+
+int main(void)
+{
+	printf ("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef ABS_H
+#define ABS_H
+#def ABS(x) ((x) < 0 ? -(x) : (x))
+#endif
