fcff4bce5c35d2d6f117524e4876aa97abed0149
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-11-15 06:12:33 +0100

    Update 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* print_dog - Prints the data elements of struct do
+* @d: Pointer to struct dog
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
