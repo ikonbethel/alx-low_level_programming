8cff657c7f7f3c221abedf31ae3723311824c40c
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-11-25 18:41:05 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,31 +1,33 @@
-#include <stdlib.h>
-#include <string.h>
-#include "strlen.c"
 #include "lists.h"
-
 /**
-* add_node - Adds a new node at the beginning of list_t list
-* @new_head: Address of node
-* @str: New node to add
-* Return: Address of new element, NULL if it fails
-*/
+ * _strlen - returns string length of input string
+ * @s: string to check length of
+ * Return: length of string
+ */
+int _strlen(const char *s)
+{
+	unsigned int len = 0;
 
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+/**
+ * add_node - adds new node to beginning of linked list
+ * @head: beginning of linked list
+ * @str: new string to be added to link list
+ * Return: pointer to new node
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node_ptr = malloc(sizeof(list_t));
+	list_t *new_node;
 
-	new_head = malloc(sizeof(list_t));
-
-	if (new_head == NULL)
-	{
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
-	}
-	else
-	{
-		new_head->str = strdup(str);
-		new_head->len = length;
-		new_head->next = *head;
-		*head = new;
-		return (new_head);
-	}
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
