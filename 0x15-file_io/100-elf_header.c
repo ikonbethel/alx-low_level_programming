deebebceb5a534074f49714fb8a547f457d74ad9
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2022-12-07 08:42:52 +0100

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -9,7 +9,47 @@
   * Return: length of a string
 
 **/
+#include <elf.h>
+#include "main.h"
+#define REV(n) ((n << 24) | (((n >> 16) << 24) >> 16) | \
+(((n << 16) >> 24) << 16) | (n >> 24))
+/**
+* verify- verify the file to check if is a ELF
+* @e_ident: the ELF struct
+* return: no return is a void func.
+*/
 
+void verify(unsigned char *e_ident)
+{
+	if (*(e_ident) == 0x7f && *(e_ident + 1) == 'E' &&
+	    *(e_ident + 2) == 'L' && *(e_ident + 3) == 'F')
+	{
+		printf("ELF Header:\n");
+	}
+	else
+	{
+		dprintf(STDERR_FILENO, "Error: this file not is a valid ELF\n");
+		exit(98);
+	}
+}
+
+/**
+* magic - print magic number
+* @e_ident: the ELF struct
+* return: no return is a void func.
+*/
+
+void magic(unsigned char *e_ident)
+{
+	int i; /* the index to count the magic bytes */
+	int limit;
+
+	limit = EI_NIDENT - 1;
+	printf("  Magic:   ");
+	for (i = 0; i < limit; i++)
+		printf("%02x ", *(e_ident + i));
+	printf("%02x\n", *(e_ident + i));
+}
 int _strlen(char *s)
 
 {
@@ -40,48 +80,35 @@ int _strlen(char *s)
 
 */
 
-int append_text_to_file(const char *filename, char *text_content)
 
-{
 
-	ssize_t nletters;
+	
 
-	int file;
+	
 
-	if (!filename)
+	
 
-		return (-1);
+		
 
-	file = open(filename, O_WRONLY | O_APPEND);
+	
 
-	if (file == -1)
+	
 
-	{
+	
 
-		return (-1);
+		
 
-	}
+	
 
-	if (text_content)
+	
 
-	{
+	
 
-		nletters = write(file, text_content, _strlen(text_content));
+		
 
-		if (nletters == -1)
+		
 
-		{
+			
 
-			close(file);
 
-			return (-1);
-
-		}
-
-	}
-
-	close(file);
-
-	return (1);
-
-}
+	
