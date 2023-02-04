911da65f60adbfcff80e654c7fb91947ee374bd7
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2023-02-04 05:56:32 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,7 +3,6 @@
 /**
  * hash_table_create - Function that creates a hash table.
  * @size: size of the array.
- *
  * Return: pointer to the hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
