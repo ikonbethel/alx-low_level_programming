e1359b96df329678dcac1f4d783da9129a795eae
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2023-12-15 04:19:32 +0100

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* set_bit - Sets the value of a bit at a given index to 1.
+* set_bit - Sets the value of a bit at a given index to 1
 * @n: A pointer to the bit.
 * @index: The index to set the value at - indices start at 0.
 *
@@ -17,4 +17,3 @@ int set_bit(unsigned long int *n, unsigned int index)
 	*n ^= (1 << index);
 
 	return (1);
-}
